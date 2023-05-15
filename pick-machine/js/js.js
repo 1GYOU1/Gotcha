9ec623490e1f416e49d8b0453e01745e3d2783b6
1GYOU1 <90018379+1GYOU1@users.noreply.github.com>
2023-05-16 00:05:54 +0900

    2023-05-15

--- pick-machine/js/js.js
+++ pick-machine/js/js.js
@@ -60,8 +60,11 @@ window.addEventListener('load', function() {
   //   console.log("드래그 요소가 '두' 번째 박스 영역에 드롭");
   // });
   
+  
+  //코인 이미지 이동, 드롭 이벤트
   var coinImg = document.querySelector(".coin_drag_area a img");
   var coinDragArea = document.querySelector(".main_area");
+  var targetElement = document.querySelector(".coin_drop_area");
   
   var active = false;
   var currentX;
@@ -81,15 +84,15 @@ window.addEventListener('load', function() {
   
   function dragStart(e) {
       if (e.type === "touchstart") {
-      initialX = e.touches[0].clientX - xOffset;
-      initialY = e.touches[0].clientY - yOffset;
+          initialX = e.touches[0].clientX - xOffset;
+          initialY = e.touches[0].clientY - yOffset;
       } else {
-      initialX = e.clientX - xOffset;
-      initialY = e.clientY - yOffset;
+          initialX = e.clientX - xOffset;
+          initialY = e.clientY - yOffset;
       }
   
       if (e.target === coinImg) {
-      active = true;
+          active = true;
       }
   }
   
@@ -98,28 +101,55 @@ window.addEventListener('load', function() {
       initialY = currentY;
   
       active = false;
+  
+      // 드래그 종료 시 특정 요소 진입 확인 함수 호출
+      checkElementEnter();
   }
   
   function drag(e) {
       if (active) {
-      
-      e.preventDefault();
-      
-      if (e.type === "touchmove") {
-          currentX = e.touches[0].clientX - initialX;
-          currentY = e.touches[0].clientY - initialY;
-      } else {
-          currentX = e.clientX - initialX;
-          currentY = e.clientY - initialY;
-      }
+          e.preventDefault();
   
-      xOffset = currentX;
-      yOffset = currentY;
+          if (e.type === "touchmove") {
+              currentX = e.touches[0].clientX - initialX;
+              currentY = e.touches[0].clientY - initialY;
+          } else {
+              currentX = e.clientX - initialX;
+              currentY = e.clientY - initialY;
+          }
   
-      setTranslate(currentX, currentY, coinImg);
+          xOffset = currentX;
+          yOffset = currentY;
+  
+          setTranslate(currentX, currentY, coinImg);
+  
+          // 드래그 중 특정 요소 진입 확인 함수 호출
+          checkElementEnter();
       }
   }
   
   function setTranslate(xPos, yPos, el) {
       el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
   }
+  
+  function handleElementEnter() {
+      console.log("Entered the target element!");
+      // 특정 요소에 진입했을 때 처리할 내용을 여기에 작성합니다.
+      // 예: 콘솔 로그, 추가 작업 등
+  }
+  
+  function checkElementEnter() {
+      var targetRect = targetElement.getBoundingClientRect();
+      var coinRect = coinImg.getBoundingClientRect();
+  
+      if (
+          coinRect.left >= targetRect.left &&
+          coinRect.right <= targetRect.right &&
+          coinRect.top >= targetRect.top &&
+          coinRect.bottom <= targetRect.bottom
+      ) {
+          // 특정 요소에 진입했을 때 처리할 함수 호출
+          handleElementEnter();
+      }
+  }
+  
