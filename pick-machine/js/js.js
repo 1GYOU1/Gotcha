3df9fb6d04e0a1108568a8b196606efda35bf530
1GYOU1 <90018379+1GYOU1@users.noreply.github.com>
2023-06-05 17:32:34 +0900

    23-06-05 모바일 버튼 css 수정, 바닐라 자바스크립트로 부분 수정

--- pick-machine/js/js.js
+++ pick-machine/js/js.js
@@ -34,37 +34,48 @@ window.addEventListener('load', function() {
 
 1. start 버튼 클릭, 메인 영역('.main_area') 노출
 
-2. for문으로 캡슐 이미지 태그 생성
+2. 랜덤 배열 생성 -> 결과 이미지
 
-3. 동전 이미지 드래그(pc), 터치(mobile) 시 메인 영역('.main_area')안에서 움직이는 효과 
+3. for문 캡슐 이미지 태그 생성
+
+4. 동전 이미지 드래그(pc), 터치(mobile) 시 메인 영역('.main_area')안에서 움직이는 효과 
    position top, left을 웹 문서 기준으로 각각 얼마나 떨어져 있는지 clientX축, clientY축 구해서 스타일 변경
 
-4. 동전 넣는 곳('.coin_drop_area') 영역에 진입했는지, 해당 영역에서 마우스를 떼는 동작을 했는지 체크
+5. 동전 넣는 곳('.coin_drop_area') 영역에 진입했는지, 해당 영역에서 마우스를 떼는 동작을 했는지 체크
 
-5. 동전 몇 번 넣었는지 카운팅 ++
+6. 동전 몇 번 넣었는지 카운팅 ++
 
-6. 동전 이미지 위치 초기화
+7. 동전 이미지 위치 초기화
 
-7. 가격과 넣은 동전의 갯수가 같을 때 애니메이션 실행
+8. 가격과 넣은 동전의 갯수가 같을 때 애니메이션 실행
 
-8. 플레이 카운트 횟수 이용해서 뽑은 캡슐 이미지 매칭
+9. 플레이 카운트 횟수 이용해서 뽑은 캡슐 이미지 매칭
 
-9. 뽑은 캡슐 클릭 시 딤처리, 확대 효과, 캡슐 오픈, 이어서 하기 버튼 클릭 이벤트
+10. 뽑은 캡슐 클릭 시 딤처리, 확대 효과, 캡슐 오픈, 이어서 하기 버튼 클릭 이벤트
 
 ++ 화살표, 이어서 계속하기 버튼 이미지 변경
 ++ 뽑았던 이미지 인벤토리 저장소
+++ 사칙연산, 퀴즈 등 맞춰야 다음 단계로 넘어가는 기능 - 문제를 선택할 수 있게 ? 제비뽑기 형식으로 만들기
+++ 모바일 사파리에서 버튼 깨지는 현상 해결
+++ 나가기 버튼
+++ 효과음 넣기
 
 */
 
-let coinDragArea = document.querySelector(".main_area");//메인 영역
-let coinImg = document.querySelector(".coin");//동전 이미지
-let coinDropArea = document.querySelector(".coin_drop_area");// 동전 넣는 영역
+// 첫 화면 영역
+let startArea = document.querySelector(".start_area");
 
-// 게임 플레이 카운트
-let playCount = 0;
+// 시작 버튼
+let startBtn = document.querySelector(".start_btn");
 
-// 동전 갯수 카운트
-let coinCount = 0;
+// 메인 영역
+let coinDragArea = document.querySelector(".main_area");
+
+// 동전 이미지
+let coinImg = document.querySelector(".coin");
+
+// 동전 넣는 영역
+let coinDropArea = document.querySelector(".coin_drop_area");
 
 // 가격
 let price = document.querySelector(".machine_area .price span").innerText;
@@ -87,6 +98,16 @@ let balls = document.querySelector(".balls");
 // 뽑은 캡슐 딤처리
 let outBallDim = document.querySelector(".capsule_open");
 
+// 랜덤 배열
+let arr = [];
+let randomArr = [];
+
+// 게임 플레이 카운트
+let playCount = 0;
+
+// 동전 갯수 카운트
+let coinCount = 0;
+
 // 드래그 이벤트 처리를 위한 변수
 let initialX = parseInt(getComputedStyle(coinImg).top);//초기 동전 이미지의 style css left 값
 let initialY = parseInt(getComputedStyle(coinImg).left);//초기 동전 이미지의 style css top 값
@@ -94,30 +115,39 @@ let currentX;//현재 동전 이미지의 left 값
 let currentY;//현재 동전 이미지의 top 값
 let active = false;//동전 이미지의 드래그인지 확인하기 위한 변수
 
-//랜덤 결과
-let arr = [];
-let randomArr = [];
-for(let i=1; i<=15; i++){
-    arr.push(i);
-}
-while(randomArr.length < 15){
-    let randomNum = Math.floor(Math.random() * 15) + 1;
-    if(!randomArr.includes(randomNum)){
-        randomArr.push(randomNum);
-    }
-}
- console.log(randomArr);
-
 //(1) start !
-$('.start_btn').click(function() {
-    $(this).parent().fadeOut();
-    setTimeout(function() {//스크롤 생기면서 버튼이 밀리는 현상때문에 setTimeout 사용
-        $('.main_area').fadeIn()
-        createBallImg()//캡슐 이미지 생성
+startBtn.addEventListener('click', startBtnEvent)
+function startBtnEvent(){
+    startBtn.classList.add('on');
+    setTimeout(function(){
+        startArea.classList.add('out');
     }, 500);
-});
+    setTimeout(function(){
+        startBtn.classList.remove('on');
+        startArea.classList.remove('out');
+        startArea.style.display = 'none';
+        coinDragArea.classList.add('on');
+        coinDragArea.style.display = 'block';
+        randomResult();//랜덤 결과 생성
+        createBallImg();//캡슐 이미지 생성
+    }, 1000);
+}
 
-//(2) 캡슐 이미지 생성
+//(2) 랜덤 결과
+function randomResult(){
+    for(let i=1; i<=15; i++){
+        arr.push(i);
+    }
+    while(randomArr.length < 15){
+        let randomNum = Math.floor(Math.random() * 15) + 1;
+        if(!randomArr.includes(randomNum)){
+            randomArr.push(randomNum);
+        }
+    }
+    console.log(randomArr);    
+}
+
+//(3) 캡슐 이미지 생성
 function createBallImg(){
     for(let i=1;i<=15;i++){
         let createBall = document.createElement("img");
@@ -128,7 +158,7 @@ function createBallImg(){
     }
 }
 
-//(3) 동전 드래그 이벤트
+//(4) 동전 드래그 이벤트
 coinDragArea.addEventListener("mousedown", dragStart);//마우스 왼쪽 버튼을 누를 때
 coinDragArea.addEventListener("mouseup", dragEnd);//마우스 왼쪽 버튼을 누르고 있다가 뗄 때
 coinDragArea.addEventListener("mousemove", drag);//마우스 왼쪽 버튼을 누르면서 움직일 때
@@ -166,7 +196,7 @@ function dragEnd() {
     checkElementEnter();//(3) coinDropArea 진입 체크
 }
 
-//(4) 동전 넣는 영역에 진입했는지 체크
+//(5) 동전 넣는 영역에 진입했는지 체크
 function checkElementEnter() {
     let targetRect = coinDropArea.getBoundingClientRect();//웹 문서상 위치 값
     let coinRect = coinImg.getBoundingClientRect();//웹 문서상 동전이미지 위치 값
@@ -182,7 +212,7 @@ function checkElementEnter() {
     }
 }
 
-//(5) 넣은 동전 카운트
+//(6) 넣은 동전 카운트
 function priceCount(){
     if(parseInt(coinCount) < parseInt(price)){//가격보다 낸 동전이 적을때만 넣을 수 있음
         coinCount += 500;
@@ -195,7 +225,7 @@ function priceCount(){
     }
 }
 
-//(6) 동전 위치 초기화
+//(7) 동전 위치 초기화
 function coinImgDisplay(){
     coinImg.style.display = 'none';
     if(parseInt(coinCount) !== parseInt(price)){//내가 낸 동전이 가격과 같을 때 제외
@@ -207,7 +237,7 @@ function coinImgDisplay(){
     }
 }
 
-//(7) 캡슐 뽑기 애니메이션 실행
+//(8) 캡슐 뽑기 애니메이션 실행
 function capsuleOut(){
     turn.classList.add('on');
     console.log('돌려 !')
@@ -232,7 +262,7 @@ function handleAni(){
     handle.removeEventListener("click", handleAni);//핸들 클릭 이벤트 제거
 }
 
-//(8) 통에 있던 캡슐이랑 이미지 매칭
+//(9) 통에 있던 캡슐이랑 이미지 매칭
 function myCapsule(){
     playCount++;//플레이 카운트 횟수 ++
     let removeBall = document.querySelector(".ball_" + playCount);
@@ -245,7 +275,7 @@ function myCapsule(){
     outBallDimEvent(outBall);//딤처리
 }
 
-//(9) 뽑은 캡슐 딤처리, 오픈, 이어서 하기 버튼
+//(10) 뽑은 캡슐 딤처리, 오픈, 이어서 하기 버튼
 function outBallDimEvent(e){
     let keepGoingbtn = document.createElement("img");
     e.addEventListener('click', () => {
@@ -253,14 +283,14 @@ function outBallDimEvent(e){
         outBallDim.append(e);
         let outBallDimImg = document.querySelector(".capsule_open.on img");
         outBallDimImg.addEventListener('click', function(){//결과 이미지 노출
-            //랜덤 결과 이미지 start
+            //랜덤 결과 이미지
             console.log('뽑은 횟수 : ',playCount);
             e.setAttribute("class", "open_ball");
-            e.setAttribute("src", "./img/open_img_" + randomArr[playCount - 1] + ".png");//랜덤 배열 기준으로 결과 이미지 노출
+            e.setAttribute("src", "./img/open_img_" + randomArr[playCount - 1] + ".png");//랜덤 배열 index 기준으로 결과 이미지 노출
             e.setAttribute("alt", "뽑은 캡슐 오픈");
             //이어서 하기 버튼 생성
             keepGoingbtn.setAttribute("class", "keep_going_btn");
-            keepGoingbtn.setAttribute("src", "./img/open_img_v2_7.png");
+            keepGoingbtn.setAttribute("src", "./img/open_img_v2_7.png");//버튼 이미지
             keepGoingbtn.setAttribute("alt", "계속");
             outBallDim.append(keepGoingbtn);
         })
@@ -268,10 +298,10 @@ function outBallDimEvent(e){
     keepGoingEvent(keepGoingbtn, e);
 }
 
-//(10) 이어서 뽑기
+//(11) 이어서 뽑기, 엔딩
 function keepGoingEvent(keepGoingbtn, item){
     keepGoingbtn.addEventListener('click', function(){
-        outBallDim.classList.remove('on');
+        outBallDim.classList.remove('on');//딤 제거
         keepGoingbtn.remove();//이어서 하기 버튼 제거
         item.remove();//뽑은 이미지 제거
         if(playCount <= 15){//이어서 뽑기
